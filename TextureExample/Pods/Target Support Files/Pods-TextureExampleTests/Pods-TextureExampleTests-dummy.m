ba70647390cf401f24fe43050e120d289bd09719
jaeyong <maxwodyd98@gmail.com>
2021-07-10 15:02:14 +0900

    Texture 초기세팅

---
+++ TextureExample/Pods/Target Support Files/Pods-TextureExampleTests/Pods-TextureExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TextureExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_TextureExampleTests
+@end
---
+++ TextureExample/TextureExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TextureExample
+//
+//  Created by 이재용 on 2021/07/10.
+//
+
+import AsyncDisplayKit
+
+class ViewController: ASDKViewController<ASDisplayNode> {
+    override init() {
+        super.init(node: ASDisplayNode())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
